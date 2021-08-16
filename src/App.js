dd44c2b75fc3b1e85a76c5a07b8631ccd033450f
adittiwari <aditya.tiwari@harman.com>
2021-08-16 23:55:58 +0530

    added todo component, dynamic list addition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import React, { useState } from 'react';
 import './App.css';
 
+import Form from "./components/Form"
+import TodoList from './components/TodoList';
+
 function App() {
+  
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className="App">
       <header>
-        Adi's to-do list
-      </header>
+        Adi's to-do list {inputText}
+      </header> 
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
+      <TodoList todos={todos}/>
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,15 @@
 import React from "react";
+import Todo from "./Todo";
 
-const TodoList = () =>{
+const TodoList = ({todos}) =>{
+    console.log(todos);
     return (
         <div className="todo-container">
-            <ul className="todo-list"></ul>
+            <ul className="todo-list">
+                {todos.map( todo => (
+                    <Todo todo={todo.text} id={todo.id}/>
+                ))}
+            </ul>
         </div>
     );
 };
